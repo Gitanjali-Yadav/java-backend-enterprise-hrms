499f21e076b1713fd071f5f4c5dacd5baa246fe3
Gitanjali Yadav <gitanjaliyadav1437@gmail.com>
2026-08-04 16:17:44 +0530

    feat: complete Sprint 0 project foundation

---
+++ backend/src/main/java/com/personalproject/common/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.personalproject.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI enterpriseHrmsOpenAPI(){
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Enterprise HRMS AP")
+                        .description("Enterprise Human Resource Management System Backend")
+                        .version("v1.0")
+                        .contact(new Contact()
+                                .name("Gitanjali Yadav")
+                                .email("gitanjaliyadav1437@gmail.com")
+                        )
+                );
+    }
+
+}
---
+++ backend/src/main/java/com/personalproject/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.personalproject.common.exception;
+
+import com.personalproject.common.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse<Void>> handleResourceNotFoundException(ResourceNotFoundException ex){
+
+        ApiResponse<Void> response =new ApiResponse<>();
+
+        response.setSuccess(false);
+        response.setMessage(ex.getMessage());
+        response.setData(null);
+        response.setTimestamp(LocalDateTime.now());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<ValidationErrorResponse>> handleValidationException(
+            MethodArgumentNotValidException ex
+    ){
+
+        Map<String, String> errors =new HashMap<>();
+        for(FieldError error : ex.getBindingResult().getFieldErrors()){
+            errors.put(error.getField(),error.getDefaultMessage());
+        }
+
+        ValidationErrorResponse validationResponse = new ValidationErrorResponse("Validation Failed" , errors
+        );
+        ApiResponse<ValidationErrorResponse> response =new ApiResponse<>();
+
+        response.setSuccess(false);
+        response.setMessage("Request Validation Failed");
+        response.setData(validationResponse);
+        response.setTimestamp(java.time.LocalDateTime.now());
+
+        return ResponseEntity.badRequest().body(response);
+    }
+}
